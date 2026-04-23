094a42d34d969fcafb473fc2f824b5cb8d5cd05f
viniciusvilelaa <ninobez@icloud.com>
2026-04-23 19:09:58 -0300

    Feat: Implementada tela de registro de usuario

--- src/pages/Register/register.jsx
+++ src/pages/Register/register.jsx
@@ -1,8 +1,78 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import '../Register/register.css';
+import { useAuth } from '../../context/apiContext';
 
 export default function Register() {
-    return (
-        <h1>Finance App - Register</h1>
-    );
-}
 
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState();
+
+  const navigate = useNavigate();
+  const { register } = useAuth();
+
+  useEffect(()=>{
+    document.body.classList.add('login-bg');
+
+    return () => {
+      document.body.classList.remove('login-bg');
+    }
+  }, []);
+
+  async function handleRegister(e) {
+    e.preventDefault();
+
+    try {
+      await register(name, email, password);
+      navigate("/home");
+    } catch (err) {
+      setError(err.message);
+    }
+  }
+
+  return (
+    <div className="login-container">
+      <div className="form-header">
+        <h1>Create Account</h1>
+        <p className="text-gray-500 text-sm">Enter your details</p>
+      </div>
+
+      <form onSubmit={handleRegister}>
+        <div className="input-container">
+          <input
+            placeholder="Name"
+            type="text"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+            required
+          />
+          <input
+            placeholder="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            required
+          />
+          <input
+            placeholder="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+
+        <button className="submit" type="submit">Registrar</button>
+      </form>
+      {error && <p style={{ color: "red" }}>{error}</p>}
+      <div className="signup">
+        Já tem uma conta? <a onClick={() => navigate("/")}>Clique aqui</a>
+      </div>
+
+      
+    </div>
+  );
+}
