f6a2e6311bfbc5ff60ac56b381f5649be4f6ad2b
viniciusvilelaa <ninobez@icloud.com>
2026-05-02 10:22:36 -0300

    Feat: Implemtado inicio do componente da pagina de historico

--- src/pages/TransactionHistory/transactionHistory.jsx
+++ src/pages/TransactionHistory/transactionHistory.jsx
@@ -4,8 +4,11 @@ export default function TransactionHistory() {
     return (
          <>
                     {/* MEIO (Ocupa 6 colunas relativas ao container de 9 colunas) */}
-                    <main className="col-span-6 mt-15 p-6 bg-white">
-                        
+                    <main className="col-span-6 mt-10 ml-2 p-6 bg-white">
+                        <div className='new-transaction-header mb-6 pl-3'>
+                            <h1 className='text-4xl font-semibold text-slate-800 mb-1'>Transaction History</h1>
+                            <p className='text-slate-500 text-sm'>A comprehensive record of your financial movements.</p>
+                        </div>
                     </main>
         
                     {/* DIREITA (Ocupa 3 colunas relativas ao container de 9 colunas) */}
--- src/pages/TransactionRegister/transactionRegister.jsx
+++ src/pages/TransactionRegister/transactionRegister.jsx
@@ -12,7 +12,7 @@ export default function TransactionRegister() {
     return (
          <>
                     {/* MEIO (Ocupa 6 colunas relativas ao container de 9 colunas) */}
-                    <main className="col-span-6 mt-10 ml-1 p-6 bg-white">
+                    <main className="col-span-6 mt-10 ml-2 p-6 bg-white">
                         <div className='new-transaction-header mb-6 pl-3'>
                             <h1 className='text-4xl font-semibold text-slate-800 mb-1'>New Transaction</h1>
                             <p className='text-slate-500 text-sm'>Log your financial moves</p>
