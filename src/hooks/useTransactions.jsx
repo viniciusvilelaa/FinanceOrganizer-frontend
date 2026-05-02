356ee1e2305c310293b37242678614160b3b638c
viniciusvilelaa <ninobez@icloud.com>
2026-05-02 11:11:38 -0300

    Feat: Reimplementada funcao hook useTrasanctions para permitir uso de filtros na requisicao get

--- src/hooks/useTransactions.jsx
+++ src/hooks/useTransactions.jsx
@@ -1,17 +1,24 @@
 import { api } from "../context/apiContext";
 import { useState, useEffect } from "react";
 
-export function useTransactions() {
-    const [transactions, setTransactions] = useState(null);
+export function useTransactions(filters = {}) {
+    const [transactions, setTransactions] = useState([]);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        api.get('/transactions')
+        setLoading(true);
+        
+        api.get('/transactions', {params:{
+            description: filters.description || undefined,
+            type: filters.type || undefined,
+            category: filters.category || undefined,
+            period: filters.period || undefined
+        }})
             .then(({data}) => setTransactions(data))
             .catch((err)=> setError(err))
             .finally(()=> setLoading(false))
-    }, []);
+    }, [filters.description, filters.type, filters.category, filters.period]);
 
     return {transactions, loading, error}
 }
