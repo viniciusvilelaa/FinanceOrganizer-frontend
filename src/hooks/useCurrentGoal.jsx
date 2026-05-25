2023daca7035a00bbb67cfb33c5c88ace83ad143
viniciusvilelaa <ninobez@icloud.com>
2026-05-25 17:15:47 -0300

    Fix(useCurrentGoal): Pequena correcao do nome da propiedade id no return

---
+++ src/hooks/useCurrentGoal.jsx
@@ -0,0 +1,49 @@
+import { api } from "../context/apiContext";
+import { useState, useEffect, useMemo } from "react";
+import axios from "axios";
+import { formatCurrency } from "../utils/formatCurrency";
+import { formatPercentage } from "../utils/formatPercentage";
+
+export function useCurrentGoal() {
+    const [currentGoal, setCurrentGoal] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState("");
+
+    useEffect(() => {
+        const controller = new AbortController();
+        const signal = controller.signal;
+
+        api.get("/goals/current", { signal })
+            .then(({ data }) => setCurrentGoal(data))
+            .catch((err) => {
+                if (!axios.isCancel(err)) {
+                    setError(err);
+                }
+            })
+            .finally(() => setLoading(false))
+
+        return () => {
+            controller.abort()
+        }
+
+    }, []);
+
+
+
+    const currentGoalEnhanced = useMemo(() => {
+        if (!currentGoal) return null;
+
+        return {
+            id: currentGoal.id,
+            targetAmount: formatCurrency(currentGoal.targetAmount),
+            currentAmount: formatCurrency(currentGoal.currentAmount),
+            percentage: formatPercentage(currentGoal.percentage),
+            month: currentGoal.month,
+            year: currentGoal.year,
+            status: currentGoal.status
+        }
+    }, [currentGoal]);
+
+    return { currentGoalEnhanced, loading, error }
+
+}
