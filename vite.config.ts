d323ca770d1f4b77df6663ac9273c5dd597046c2
viniciusvilelaa <ninobez@icloud.com>
2026-05-28 11:31:52 -0300

    Feat: Implementado proxy no arquivo de configuracao do vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // 1. Adicione a importação de volta
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss() // 2. Adicione o plugin de volta aqui
+  ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+        secure: true,
+      }
+    }
+  }
 })
