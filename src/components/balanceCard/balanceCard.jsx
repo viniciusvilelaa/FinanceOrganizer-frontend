2780b154e1cba2db9f50551848f80ab7bfa6c8b0
viniciusvilelaa <ninobez@icloud.com>
2026-05-29 10:29:25 -0300

    Refat(balanceCard): Removido imports desnecessarios

--- src/components/balanceCard/balanceCard.jsx
+++ src/components/balanceCard/balanceCard.jsx
@@ -1,9 +1,7 @@
 import '../balanceCard/balanceCard.css'
-import { useNavigate } from 'react-router-dom';
 import { formatCurrency } from '../../utils/formatCurrency';
 
 export default function BalanceCard({ total }) {
-  const navigate = useNavigate();
   return (
     <div className="bg-white rounded-xl p-[25px] text-left  w-full card-container">
       <h1 className="text-sm font-normal text-gray-400 mb-2 uppercase tracking-wider">Total Balance</h1>
