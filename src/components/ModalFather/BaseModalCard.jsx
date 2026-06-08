fd4915544bfe2daa44ab0e702799bf458708cc71
viniciusvilelaa <ninobez@icloud.com>
2026-06-08 09:24:33 -0300

    Feat(BaseModal): Implementado componente base para os modais da aplicacao

---
+++ src/components/ModalFather/BaseModalCard.jsx
@@ -0,0 +1,44 @@
+import { useRef, useEffect } from "react"
+import { X } from "lucide-react";
+
+export default function BaseModalCard({ isOpen, onClose, children, title }) {
+    const dialogRef = useRef(null);
+
+    useEffect(() => {
+        const dialog = dialogRef.current;
+
+        if(!dialog) return null
+
+        if(isOpen){
+            dialog.showModal();
+            document.body.style.overflow = 'hidden';
+        }else {
+            dialog.close();
+            document.body.style.overflow = ""
+        }
+
+        return () => {
+            document.body.style.overflow = ""
+        }
+
+     }, [isOpen]);
+
+
+    return (
+        <dialog ref={dialogRef} onClose={onClose} className="bg-transparent border-none backdrop:bg-black/50">
+            <div className="fixed inset-0 z-40" onClick={onClose}></div>
+
+            <div className="relative z-50 bg-white rounded-2xl shadow-x1 p-6 w-full max-w-md" onClick={(e)=> e.stopPropagation()}>
+                <div className="flex items-center justify-between mb-6">
+                    <h2 className="text-lg font-semibold text-gray-800">{title}</h2>
+                    <button onClick={onClose} className="text-gray400 hover:text-gray-600 transition-colors"> 
+                        <X className="w-5 h-5" ></X>
+                    </button>
+                </div>
+
+                {children}
+            </div>
+
+        </dialog>
+    );
+}
