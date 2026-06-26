f55e7b212c646cc1060d0b398bd8ff6cd0939d35
viniciusvilelaa <ninobez@icloud.com>
2026-06-26 11:20:55 -0300

    Refat(transactionHistory): Alterado margi top do componente pai para 1

--- src/pages/TransactionHistory/transactionHistory.jsx
+++ src/pages/TransactionHistory/transactionHistory.jsx
@@ -5,7 +5,7 @@ export default function TransactionHistory() {
     return (
          <>
                     {/* MEIO (Ocupa 9 colunas relativas ao container de 9 colunas) */}
-                    <main className="col-span-9 mt-10 ml-2 p-6 bg-white">
+                    <main className="col-span-9 mt-1 ml-2 p-6 bg-white">
                         <div className='new-transaction-header mb-6 pl-3'>
                             <h1 className='text-4xl font-semibold text-slate-800 mb-1'>Transaction History</h1>
                             <p className='text-slate-500 text-sm'>A comprehensive record of your financial movements.</p>
