49da83d068e9812a7c6a36043cfe10dd3589114b
viniciusvilelaa <ninobez@icloud.com>
2026-07-11 12:23:24 -0300

    Feat(useCreateTransaction): Implementado hook de createTransaction para utilizar o mutate e alterar o cache das outras queries

---
+++ src/hooks/useCreateTransaction.jsx
@@ -0,0 +1,27 @@
+import { api } from "../context/apiContext";
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
+import { QUERY_KEYS } from "../queryKeys/queryKeys";
+import { toast } from "sonner";
+
+const fetchCreateTransaction = async ({type, amount, date, category, description}) => {
+    const payload = {amount: parseFloat(amount), type, date, category, description};
+    
+    const {data} = await api.post('/transactions', payload);
+
+    return data
+}
+
+export function useCreateTransaction(){
+    const queryClient = useQueryClient();
+
+    const {mutateAsync, isPending} = useMutation({
+        mutationFn: fetchCreateTransaction,
+        onSuccess: () => {
+            queryClient.invalidateQueries({queryKey: QUERY_KEYS.finance});
+            toast.success("Transaction Created");
+        },
+        onError: (error) => {toast.error(error.response?.data?.message || "Error to create transaction")}
+    });
+
+    return{createTransaction: mutateAsync, isCreating: isPending}
+}
